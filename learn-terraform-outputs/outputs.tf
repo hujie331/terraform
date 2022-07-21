8f73f02ece0c19f239f3ff7659805edbcca7f4c5
letmetechyou <dcwbys276@live.com>
2022-07-21 00:09:19 -0400

    add secure output for db

--- learn-terraform-outputs/outputs.tf
+++ learn-terraform-outputs/outputs.tf
@@ -2,7 +2,7 @@
 
 output "vpc_id" {
   description = "id of vpc"
-  value       = aws_vpc.vpc.id
+  value       = module.vpc.vpc_id
 }
 
 output "lb_url" {
@@ -13,4 +13,16 @@ output "lb_url" {
 output "web_server_count" {
     description = "number of web servers provisioned"
     value       = length(module.ec2_instances.instance_ids)
+}
+
+output "db_username" {
+    description = "username for database"
+    value       = "aws_db_instance.database.username"
+    sensitive   = true
+}
+
+output "db_password" {
+    description = "password for database"
+    value       = "aws_db_instance.database.password"
+    sensitive   = true
 }
