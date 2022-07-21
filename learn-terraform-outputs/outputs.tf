4766be088368d655b5c7fad36f9fcbb53e9387cf
letmetechyou <dcwbys276@live.com>
2022-07-20 22:53:02 -0400

    add outputs

--- learn-terraform-outputs/outputs.tf
+++ learn-terraform-outputs/outputs.tf
@@ -1,1 +1,16 @@
 # Output declarations
+
+output "vpc_id" {
+  description = "id of vpc"
+  value       = aws_vpc.vpc.id
+}
+
+output "lb_url" {
+    description = "URL of load balancer"
+    value       = aws_lb.lb.dns_name
+}
+
+output "web_server_count" {
+    description = "number of web servers provisioned"
+    value       = length(module.ec2_instances.instance_ids)
+}
