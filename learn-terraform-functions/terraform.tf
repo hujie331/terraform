da367f664beab78a245d576b985519b680c5add6
letmetechyou <dcwbys276@live.com>
2022-08-03 02:10:19 -0400

    initial commit

---
+++ learn-terraform-functions/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  
+  cloud {
+    organization = "ysugrad"
+    workspaces {
+      name = "learn-terraform-functions"
+    }
+  }
+  
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
